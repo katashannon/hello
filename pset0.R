15eac0da6a3a0e087d744058b3f2d6dee2120126
Kat Adams Shannon <katashannon@gmail.com>
2021-10-01 16:19:54 -0700

    add line after remobed DS_Store file

--- pset0.R
+++ pset0.R
@@ -7,3 +7,4 @@ print("hello world")
 #making more changes
 #test after moved directory location
 #test 2 after moved directory location and deleted other folder
+#test after trying to delete DS Store
